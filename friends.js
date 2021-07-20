8c05a2cd44f8622e724c8fb24b1be0f7e6455d06
Aysan Isayo <aisayo@icloud.com>
2021-07-19 20:12:23 -0700

    added html

---
+++ friends.js
@@ -0,0 +1,39 @@
+const friends = {
+    genre: 'sitcom',
+    created_by: ['David Crane', 'Marta Kauffman'],
+    opening_theme: '"Ill Be There for You" by The Rembrandts',
+    seasons: 10,
+    release_date: 'September 22, 1994',
+    characters: [
+        {
+            char_name: 'Rachel Greene',
+            actor: 'Jennifer Aniston',
+            profession: 'Fashion Assistant Buyer'
+        },
+        {
+            char_name: 'Monica Geller',
+            actor: 'Courtney Cox',
+            profession: 'Chef'
+        },
+        {
+            char_name: 'Phoebe Buffay',
+            actor: 'Lisa Kudrow',
+            profession: 'Masseuse'
+        },
+        {
+            char_name: 'Ross Geller',
+            actor: 'David Schwimmer',
+            profession: 'Paleontologist'
+        },
+        {
+            char_name: 'Joey Tribbiani',
+            actor: 'Mat LeBlanc',
+            profession: 'Aspiring Actor'
+        },
+        {
+            char_name: 'Chandler Bing',
+            actor: 'Matthew Perry',
+            profession: 'Statistical Analysis'
+        },
+    ]
+}
